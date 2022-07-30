3efc3a4616eb81a991031c669327b4a3f4acbf32
Angela Caldas <aes.caldas@gmail.com>
2022-07-30 16:41:36 -0300

    gallery component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,12 @@
 import { images } from "../data/imgs";
 
+import '../styles/components/gallery.sass';
+
 const Gallery = () => {
     return (
         <session className='gallery'>
             {images.map((image, i) => (
-                <div className="gallery__card" id={`card-${i + 1}`}>
+                <div className="gallery__card" id={`card-${i + 1}`} style={{ backgroundImage: `url(${image.src})` }}>
                     <h2 className="gallery__card--title">{image.name}</h2>
                     <a href="#" className="gallery__card--btn">Mais fotos</a>
                 </div>
