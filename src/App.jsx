4099f71ec75e833fe4a60387a33042428a87ed11
Angela Caldas <aes.caldas@gmail.com>
2022-07-30 14:53:32 -0300

    init and first config

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// components
+import Navbar from "./components/Navbar";
+
+function App() {
+
+  return (
+    <main className="App">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
